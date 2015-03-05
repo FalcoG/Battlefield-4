6d775b9964fe3c0c939bebfc5e6afc69e571f173
FalcoG <falco@gerritsjans.com>
2015-03-05 20:33:10 +0100

    PHP GD basic test

--- index.php
+++ index.php
@@ -12,23 +12,9 @@
 <img src="profile.php">
 
 <?php
-/*
-$file = fopen("http://api.bf4stats.com/api/playerInfo?plat=pc&name=I_EAT_TANKS&output=js", "r");
-if (!$file) {
-    echo "<p>Unable to open remote file.\n";
-    exit;
-}
-*/
-//$file = file_get_contents("http://api.bf4stats.com/api/playerInfo?plat=pc&name=I_EAT_TANKS&output=json");
 $file = file_get_contents("playerInfo.json");
 
-//echo $file;
 var_dump(json_decode($file));
-/*
-while (!feof ($file)) {
-    var_dump($file);
-}
-*/
 ?>
 </body>
 </html>
