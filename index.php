0d9b988318d39a1b079e2cd3b2e998e49c5435cf
FalcoG <falco@gerritsjans.com>
2015-03-05 23:06:48 +0100

    PHP GD basic test

--- index.php
+++ index.php
@@ -12,7 +12,11 @@
 <img src="profile.php">
 
 <?php
-$file = file_get_contents("playerInfo.json");
+$file = file_get_contents("http://api.bf4stats.com/api/playerInfo?plat=pc&name=I_EAT_TANKS&output=json");
+//$file = file_get_contents("playerInfo.json");
+$stats = json_decode($file);
+
+echo $stats->player->id;
 
 var_dump(json_decode($file));
 ?>
--- profile.php
+++ profile.php
@@ -1,10 +1,22 @@
 <?php
+//$file = file_get_contents("http://api.bf4stats.com/api/playerInfo?plat=pc&name=I_EAT_TANKS&output=json");
+$username = "I_EAT_TANKS";
+$file = file_get_contents("playerInfo.json");
+$stats = json_decode($file);
+
 function imagettfstroketext(&$image, $size, $angle, $x, $y, &$textcolor, &$strokecolor, $fontfile, $text, $px) {
     for($c1 = ($x-abs($px)); $c1 <= ($x+abs($px)); $c1++)
         for($c2 = ($y-abs($px)); $c2 <= ($y+abs($px)); $c2++)
             $bg = imagettftext($image, $size, $angle, $c1, $c2, $strokecolor, $fontfile, $text);
 
     return imagettftext($image, $size, $angle, $x, $y, $textcolor, $fontfile, $text);
+}
+
+function imagettftextcenter($image, $font_size, $angle, $x, $y, $color, $font, $string){
+    $type_space = imagettfbbox($font_size, 0, $font, $string);
+    $text_width = abs($type_space[4] - $type_space[0]);
+
+    imagettftext($image, $font_size, $angle, $x-($text_width/2), $y, $color, $font, $string);
 }
 
 header("Content-Type: image/png");
@@ -15,24 +27,53 @@ imagefill($im, 0, 0, $color);
 imagesavealpha($im, true);
 
 $afta = "fonts/AftaSansThin-Regular.ttf";
-$arial = "fonts/arial.ttf";
+$Arial = "fonts/arial.ttf";
+$ArialBold = "fonts/arialbd.ttf";
+$PuristaSemibold = "fonts/PuristaSemibold.ttf";
+$PuristaBold = "fonts/PuristaBold.ttf";
 
 $white = imagecolorallocate($im, 255, 255, 255);
 $black = imagecolorallocate($im, 0, 0, 0);
 $green = imagecolorallocate($im, 0, 255, 0);
 
-//imagecolortransparent($im, $white);
+$grey = imagecolorallocate($im, 168, 168, 168);
 
 $bg = imagecreatefrompng("images/recon.png");
 $emblem = imagecreatefrompng("images/emblem.png");
-imagecopyresized($im, $emblem, 50, 50, 0, 0, 128, 128, 256, 256);
+imagecopyresized($im, $emblem, 50, 95, 0, 0, 128, 128, 256, 256);
 imagecopy($im, $bg, 161, 0, 0, 0, 297, 336);
 
-imagettftext($im, 16, 0, 15, 30, $white, $afta, "[RAGE]I_EAT_TANKS");
-imagettftext($im, 16, 0, 15, 250, $green, $afta, $_SERVER["REMOTE_ADDR"]);
-//imagettftext($im, 16, 0, 15, 250, $green, $arial, "Testing a lot of text");
-imagettfstroketext($im, 24, 0, 15, 280, $green, $black, $afta, "Testing a lot of text", 1);
+imagettftext($im, 16, 0, 15, 95, $white, $Arial, "[" . $stats->player->tag . "] " . $username);
+/*
+/* --------------- BATTLEFIELD STATS ----------------- */
+
+imagettftextcenter($im, 10, 0, 470, 65, $grey, $Arial, "K/D");
+imagettftextcenter($im, 16, 0, 470, 95, $white, $PuristaSemibold, round($stats->stats->extra->kdr, 2, PHP_ROUND_HALF_DOWN));
+
+imagettftextcenter($im, 10, 0, 560, 65, $grey, $Arial, "SPM");
+imagettftextcenter($im, 16, 0, 560, 95, $white, $PuristaSemibold, floor($stats->stats->extra->spm));
+
+/* --------------------------------------------------- */
+
+imagettftextcenter($im, 10, 0, 470, 140, $grey, $Arial, "KILLS");
+imagettftextcenter($im, 12, 0, 470, 170, $white, $PuristaSemibold, $stats->stats->kills);
+
+imagettftextcenter($im, 10, 0, 560, 140, $grey, $Arial, "KPM");
+imagettftextcenter($im, 16, 0, 560, 170, $white, $PuristaSemibold, round($stats->stats->extra->kpm, 2, PHP_ROUND_HALF_DOWN));
+
+/* --------------------------------------------------- */
+
+imagettftextcenter($im, 10, 0, 470, 215, $grey, $Arial, "SCORE");
+imagettftextcenter($im, 10, 0, 470, 235, $white, $PuristaSemibold, $stats->player->score);
+
+$seconds = $stats->player->timePlayed;
+$time = floor($seconds/3600). "h " . gmdate("i", $seconds) . "m ";
+
+imagettftextcenter($im, 10, 0, 560, 215, $grey, $Arial, "TIME");
+imagettftextcenter($im, 10, 0, 560, 235, $white, $PuristaSemibold, $time);
+
 
 imagepng($im);
 imagedestroy($im);
+
 ?>
