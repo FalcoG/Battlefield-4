c9131c9bb4c406ee985293c93827b7310ccb7437
Falco Gerritsjans <falco@gerritsjans.com>
2015-03-05 18:42:42 +0100

    PHP GD basic test

--- profile.php
+++ profile.php
@@ -1,9 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Falco
- * Date: 5-3-2015
- * Time: 18:34
- */
-// http://d34ymitoc1pg7m.cloudfront.net/bf4/soldier/large/us-recon-eccec0dd.png
-echo "Test";
+header("Content-Type: image/png");
+$im = imagecreate(620, 300);
+$font = "fonts/AftaSansThin-Regular.ttf";
+
+$white = imagecolorallocate($im, 255, 255, 255);
+$black = imagecolorallocate($im, 0, 0, 0);
+$green = imagecolorallocate($im, 0, 255, 0);
+
+imagecolortransparent($im, $white);
+
+$bg = imagecreatefrompng("images/recon.png");
+imagecopy($im, $bg, 161, 0, 0, 0, 297, 336);
+
+imagettftext($im, 16, 0, 15, 30, $black, $font, "I_EAT_TANKS");
+imagettftext($im, 16, 0, 15, 280, $black, $font, $_SERVER["REMOTE_ADDR"]);
+
+imagepng($im);
+imagedestroy($im);
+?>
